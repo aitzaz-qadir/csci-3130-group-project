8e1a1aa0b46c5782bcd70800220fed38effbb4da
akhowaja <at382154@dal.ca>
2021-06-10 19:41:09 -0300

    Added Blob Detection

--- content.js
+++ content.js
@@ -1,6 +1,16 @@
 // Javascript that will run in context to visited website.
 
 var main = document.getElementById("container")
-var links = document.getElementsByTagName("video");
+var links = document.getElementsByTagName("embed")
 console.log("Hey this is my output;")
-console.log(links)
+console.log(vid_find())
+
+function vid_find() {
+	var vids = document.getElementsByTagName("video");
+	var vidSrcs = [];
+	for (var i = 0; i < vids.length; i++) {
+		vidSrcs.push(vids[i].src);
+	}
+
+	return vidSrcs;
+}
