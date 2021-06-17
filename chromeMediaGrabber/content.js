e520a7010dcaa662288d18604991a9aa5b586412
akhowaja <at382154@dal.ca>
2021-06-17 12:49:03 -0300

    Added content.js

---
+++ chromeMediaGrabber/content.js
@@ -0,0 +1,21 @@
+// Javascript that will run in context to visited website.
+
+var main = document.getElementById("container")
+var links = document.getElementsByTagName("embed")
+console.log("Hey this is my output;")
+console.log(vid_find())
+
+
+function vid_find() {
+	var vids = document.getElementsByTagName("video");
+	var vidSrcs = [];
+	for (var i = 0; i < vids.length; i++) {
+		vidSrcs.push(vids[i].src);
+	}
+
+	return vidSrcs;
+}
+
+// function download() {
+	
+// }
