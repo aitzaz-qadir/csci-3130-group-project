181e9a1b6f75131e20dc59b180a1f93550d8c447
akhowaja <at382154@dal.ca>
2021-06-17 13:13:09 -0300

    Added Allen's files

--- chromeMediaGrabber/background.js
+++ chromeMediaGrabber/background.js
@@ -1,5 +1,17 @@
+// James - JS
 let color = '#3aa757';
 	chrome.runtime.onInstalled.addListener(() => {
 	chrome.storage.sync.set({ color });
 	console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+// Wangwenzan Zhou - JS
+chrome.tabs.onActivated.addListener(tab =>{
+    chrome.tabs.get(tab.tabId, current_tab_info =>{
+        if(/^http:\/\/www\.youyube/.test(current_tab_info.url)){
+            chrome.tabsinsertCSS(null,{file:'./main.css'});
+            chrome.tabs.executeScript(null, {File: './foreground.js'},() => console.log('i injected'))
+        }
+    });
+});
+
