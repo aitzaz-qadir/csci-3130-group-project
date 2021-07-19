5054560cb68f430f10a89456e636fa0e8b36935e
Brynn <brynn.haley@live.com>
2021-07-19 15:27:26 -0300

    Get background.js to communicate with popup.js

--- chromeMediaGrabber/background.js
+++ chromeMediaGrabber/background.js
@@ -1,10 +1,6 @@
-// Wangwenzan Zhou - JS
-chrome.tabs.onActivated.addListener(tab =>{
-    chrome.tabs.get(tab.tabId, current_tab_info =>{
-        if(/^http:\/\/www\.youyube/.test(current_tab_info.url)){
-            chrome.tabsinsertCSS(null,{file:'./main.css'});
-            chrome.tabs.executeScript(null, {File: './foreground.js'},() => console.log('i injected'))
-        }
-    });
+console.log("we are here");
+
+chrome.scripting.executeScript({
+    file: 'popup.js'
 });
 
--- chromeMediaGrabber/scripts/popup.js
+++ chromeMediaGrabber/scripts/popup.js
@@ -1,28 +1,8 @@
-// Import libraries for downloading videos
-const fs = require('fs')
-const youtubedl = require('youtube-dl')
-
 var downloadBtn = document.querySelector('.download-btn');
-var url = document.querySelector('.input-url');
 
-downloadBtn.addEventListener('click', () => {
-    console.log(`URL: ${url.value}`);
-    download();
-});
+downloadBtn.addEventListener('click', download);
 
-def download() {
-    // Create downloadable video object from user-inputted url 
-    const video = youtubedl('url.value',
-        // Optional arguments passed to youtube-dl.
-        ['--format=18']
-    )
+function download() {
+    alert("download function has been called!")
 
-    // Will be called when the download starts.
-    video.on('info', function (info) {
-        console.log('Download started')
-        console.log('filename: ' + info._filename)
-        console.log('size: ' + info.size)
-    })
-
-    video.pipe(fs.createWriteStream('myvideo.mp4'))
 }
