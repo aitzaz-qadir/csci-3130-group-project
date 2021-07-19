9d9ff574e21037c5fa067c6b1f11780b0a75af66
Brynn <brynn.haley@live.com>
2021-07-19 15:32:42 -0300

    Get text in input field

--- chromeMediaGrabber/scripts/popup.js
+++ chromeMediaGrabber/scripts/popup.js
@@ -1,8 +1,12 @@
+// Get DOM elements from popup.html
 var downloadBtn = document.querySelector('.download-btn');
+var input = document.querySelector('.input-url');
 
+// Add event listener to download button to initiate download 
 downloadBtn.addEventListener('click', download);
 
+// Called when download button is clicked
 function download() {
-    alert("download function has been called!")
-
+    link = input.value; // Get video link from input field
+    alert(link);
 }
