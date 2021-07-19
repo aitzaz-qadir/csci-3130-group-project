16ce13321949f579bcd6566af3826d12f90d26e4
Brynn <brynn.haley@live.com>
2021-07-19 16:16:11 -0300

    Files can now be downloaded

--- chromeMediaGrabber/scripts/popup.js
+++ chromeMediaGrabber/scripts/popup.js
@@ -3,10 +3,23 @@ var downloadBtn = document.querySelector('.download-btn');
 var input = document.querySelector('.input-url');
 
 // Add event listener to download button to initiate download 
-downloadBtn.addEventListener('click', download);
+downloadBtn.addEventListener('click', initDownload);
 
-// Called when download button is clicked
-function download() {
-    link = input.value; // Get video link from input field
-    alert(link);
+// Called when download button is clicked 
+function download(url, fileName) {
+
+    // Create an invisible element with url 
+    var a = document.createElement("a");
+    a.href = url; 
+    a.setAttribute("download", fileName);
+    a.click();
+
+    alert(url);
+}
+
+function initDownload() {
+    url = input.value;
+    fileName = "myFile"
+
+    download(url, fileName);
 }
