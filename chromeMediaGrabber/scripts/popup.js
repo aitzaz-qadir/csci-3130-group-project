9865acd4cb28291555a20502f110b4152843d29c
Brynn <brynn.haley@live.com>
2021-07-19 14:14:03 -0300

    Get service worker working for debugging

--- chromeMediaGrabber/scripts/popup.js
+++ chromeMediaGrabber/scripts/popup.js
@@ -1,16 +1,28 @@
-// Storing the video division into a variable
-arrList = document.getElementById("vidfind");
-// Storing the first blob from array as string
-blob = srcsArray[0]
+// Import libraries for downloading videos
+const fs = require('fs')
+const youtubedl = require('youtube-dl')
 
+var downloadBtn = document.querySelector('.download-btn');
+var url = document.querySelector('.input-url');
 
-if (srcsArray.length > 0) {
-	console.log(blob)
-	arrList = document.getElementById("vidfind");
-	arrList.innerHTML = blob;
-}
-else {
-	console.log("blob")
-	arrList = document.getElementById("vidfind");
-	arrList.innerHTML = "blob" ;
+downloadBtn.addEventListener('click', () => {
+    console.log(`URL: ${url.value}`);
+    download();
+});
+
+def download() {
+    // Create downloadable video object from user-inputted url 
+    const video = youtubedl('url.value',
+        // Optional arguments passed to youtube-dl.
+        ['--format=18']
+    )
+
+    // Will be called when the download starts.
+    video.on('info', function (info) {
+        console.log('Download started')
+        console.log('filename: ' + info._filename)
+        console.log('size: ' + info.size)
+    })
+
+    video.pipe(fs.createWriteStream('myvideo.mp4'))
 }
